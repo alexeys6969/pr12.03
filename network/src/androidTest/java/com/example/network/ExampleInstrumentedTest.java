b73819798ac3429bc09ddf95cad2edebc46388e6
gschhh <alexshashin.2023@gmail.com>
2026-03-28 17:19:31 +0500

    Первый разработанный тест

--- network/src/androidTest/java/com/example/network/ExampleInstrumentedTest.java
+++ network/src/androidTest/java/com/example/network/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.network;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -10,6 +11,12 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.example.network.datas.users.UserLogin;
+import com.example.network.domains.callbacks.MyResponseCallback;
+import com.example.network.domains.models.User;
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -18,9 +25,29 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.network.test", appContext.getPackageName());
+    public void UserLogin() throws InterruptedException {
+        final Boolean[] Success = {false};
+        CountDownLatch Latch = new CountDownLatch(1);
+        User User = new User("testing@mail.ru", "Asdfg123*");
+
+        new UserLogin(
+                User,
+                new MyResponseCallback() {
+                    @Override
+                    public void onCompile(String result) {
+                        Log.d("USER LOGIN", result);
+                        Success[0] = true;
+                        Latch.countDown();
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("USER LOGIN", error);
+                        Latch.countDown();
+                    }
+                }
+        ).execute();
+        Boolean Completed = Latch.await(60, TimeUnit.SECONDS);
+        assertTrue(Success[0]);
     }
 }
