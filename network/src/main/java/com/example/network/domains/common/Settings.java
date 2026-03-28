72f533e2fdffe2a008e99202af324bce698aef02
gschhh <alexshashin.2023@gmail.com>
2026-03-28 16:41:13 +0500

    Разработанная модель данных. Шашин

---
+++ network/src/main/java/com/example/network/domains/common/Settings.java
@@ -0,0 +1,6 @@
+package com.example.network.domains.common;
+
+public class Settings {
+    public static String URL = "http://10.111.20.114:5000";
+    public static String DEMO_TOKEN = "билиберда";
+}
