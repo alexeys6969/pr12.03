4d61c0872bb9ced59623884461015068fdea9b5f
gschhh <alexshashin.2023@gmail.com>
2026-03-31 16:04:00 +0500

    ddd

--- network/src/main/java/com/example/network/domains/common/Settings.java
+++ network/src/main/java/com/example/network/domains/common/Settings.java
@@ -2,5 +2,5 @@ package com.example.network.domains.common;
 
 public class Settings {
     public static String URL = "http://10.111.20.114:5000";
-    public static String DEMO_TOKEN = "билиберда";
+    public static String DEMO_TOKEN = "010615ea-21c8-40b8-a3af-80fa16b30bb1";
 }
